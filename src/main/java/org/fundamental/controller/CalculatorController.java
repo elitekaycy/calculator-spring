7a66cb7e317a8d348e087391477934f3f7c9f693
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-02-06 10:19:19 +0000

    feat: added calculator controller

--- src/main/java/org/fundamental/controller/CalculatorController.java
+++ src/main/java/org/fundamental/controller/CalculatorController.java
@@ -1,2 +1,59 @@
-package org.fundamental.controller;public class CalculatorController {
+package org.fundamental.controller;
+
+import org.fundamental.dto.CalculatorApiResponse;
+import org.fundamental.service.calculator.CalculatorServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CalculatorController {
+
+    private final CalculatorServiceImpl calculatorService;
+
+    public CalculatorController(CalculatorServiceImpl calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/add")
+    public ResponseEntity<CalculatorApiResponse> addCalculation(@RequestParam(name = "operands") List<Double> operands) throws Exception {
+        CalculatorApiResponse result = calculatorService.add(operands);
+        return ResponseEntity.ok(result);
+    };
+
+    @GetMapping("/subtract")
+    public ResponseEntity<CalculatorApiResponse> subtractCalculation(@RequestParam(name = "operands") List<Double> operands) throws Exception {
+        CalculatorApiResponse result = calculatorService.subtract(operands);
+        return ResponseEntity.ok(result);
+    };
+
+    @GetMapping("/multiply")
+    public ResponseEntity<CalculatorApiResponse> multiplyCalculation(@RequestParam(name = "operands") List<Double> operands) throws Exception {
+        CalculatorApiResponse result = calculatorService.multiply(operands);
+        return ResponseEntity.ok(result);
+    };
+
+    @GetMapping("/divide")
+    public ResponseEntity<CalculatorApiResponse> divideCalculation(@RequestParam(name = "operands") List<Double> operands) throws Exception {
+        CalculatorApiResponse result = calculatorService.divide(operands);
+        return ResponseEntity.ok(result);
+    };
+
+    @PostMapping("/reset")
+    public ResponseEntity<CalculatorApiResponse> resetCalculation() throws Exception {
+        CalculatorApiResponse result = calculatorService.reset();
+        return ResponseEntity.ok(result);
+    };
+
+    @PostMapping("/clear")
+    public ResponseEntity<CalculatorApiResponse> divideCalculation() throws Exception {
+        CalculatorApiResponse result = calculatorService.clearHistory();
+        return ResponseEntity.ok(result);
+    };
+
+
 }
